b543bc697f1183ea1a5fac3bb295eb74fbaba78c
Imre Toth <tothimre@designtech.hu>
2021-03-07 20:22:00 +0100

    Updated config route (refact)

--- app/routes/config.routes.js
+++ app/routes/config.routes.js
@@ -2,14 +2,12 @@ module.exports = app => {
   const router = require('express').Router();
 
   // Retrieve all columns (@todo - dynamic)
-  router.get('/config', (req, res) => {
-    return res.json({
-      columns: {
-        categories: ['name'],
-        costs: ['categoryId', 'date', 'value'],
-      },
-    });
-  });
+  router.get('/config', (req, res) => res.json({
+    columns: {
+      categories: ['name'],
+      costs: ['categoryId', 'date', 'value'],
+    },
+  }));
 
   app.use('/api', router);
 };
